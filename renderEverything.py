4e86adad0f95a81b99c9a541d63a71a79af342fe
Lawrence Lee <lawrenceleejr@gmail.com>
2024-11-02 16:52:23 -0400

    fixed dof loop

--- renderEverything.py
+++ renderEverything.py
@@ -10,6 +10,20 @@ physicsObjects = ["Muon","Neutron","Photon","All"]
 basepath = "/Users/leejr/work/MuonColliderBlender/"
 
 
+
+def render_callback(scene):
+    # Check if rendering is in progress
+    if bpy.data.scenes[0].render.is_rendering:
+        print("Rendering in progress...")
+    else:
+        print("Rendering complete.")
+        return None  # Stop the timer
+
+    return 1.0  # Check again in 1 second
+
+# Register the callback to check render status every second
+bpy.app.timers.register(render_callback)
+
 print("starting loop")
 
 for focusObject in physicsObjects:
@@ -69,36 +83,35 @@ focusDistances = [
     8100, #neutron
     12000, #blurry
     ]
-
-for focusDistance in focusDistances:
-    print("------",focusDistance)
-
-    # Enable all physics objects
-    for obj_name in physicsObjects[:-1]:
-        obj = bpy.data.objects.get(obj_name)
-        if obj:
-            obj.hide_render = False
-        else:
-            print(f"Object '{obj_name}' not found.")
-
-    cameras_to_enable = ["_OrthoDOF",]
-
-    # Enable multi-view and set specific cameras
-    bpy.context.scene.render.use_multiview = True
-
-    for view in bpy.context.scene.render.views:
-        view.use = False
-        if view.name in cameras_to_enable:
-            view.use = True
-
-    cam = bpy.data.objects.get("Camera_OrthoDOF")
-    cam.data.lens = focusDistance
-
-    # Set the render path and output settings
-    output_path = f"{basepath}/{timestamp_str}_Render_All_Ortho_{focusDistance}.png"
-    bpy.context.scene.render.filepath = output_path
-
-    # Perform the render
-    bpy.ops.render.render(animation=False, write_still=True)
+# not using the lists in the end since I can't change the focus distance in a script (arggggg)
 
 
+# Enable all physics objects
+for obj_name in physicsObjects[:-1]:
+    obj = bpy.data.objects.get(obj_name)
+    if obj:
+        obj.hide_render = False
+    else:
+        print(f"Object '{obj_name}' not found.")
+
+cameras_to_enable = [
+    "_OrthoDOF5500",
+    "_OrthoDOF6300",
+    "_OrthoDOF8100",
+    "_OrthoDOF12000",    
+    ]
+
+# Enable multi-view and set specific cameras
+bpy.context.scene.render.use_multiview = True
+
+for view in bpy.context.scene.render.views:
+    view.use = False
+    if view.name in cameras_to_enable:
+        view.use = True
+
+# Set the render path and output settings
+output_path = f"{basepath}/{timestamp_str}_Render_All_Ortho.png"
+bpy.context.scene.render.filepath = output_path
+
+# Perform the render
+bpy.ops.render.render(animation=False, write_still=True)
